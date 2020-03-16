8937988bde54d30b172fe7c18de61ea7fc2e4edc
SergDm <dseralex@gmail.com>
2020-03-16 14:54:42 +0200

    delete key

--- keys/index.js
+++ keys/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGODB_URI: 'mongodb+srv://sergeydm:v9q4J0i01bWbPSs9@cluster0-11lpy.mongodb.net/shop',
+  MONGODB_URI: '',
   SESSION_SECRET: 'some secret',
   SENDGRID_API_KEY: '',
   EMAIL_FROM: 'nodejs-shop@gmail.com',
